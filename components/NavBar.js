c16f79762d1a7403875640de77ec46067097fc37
pjh5474 <warwarsn@gmail.com>
2023-11-14 12:03:03 +0900

    CSS Modules

---
+++ components/NavBar.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import styles from "./NavBar.module.css";
+
+export default function NavBar() {
+  const router = useRouter();
+  return (
+    <nav>
+      <Link
+        className={`${styles.link} ${
+          router.pathname === "/" ? styles.active : ""
+        }`}
+        href="/"
+      >
+        Home
+      </Link>
+      <Link
+        className={[
+          styles.link,
+          router.pathname === "/about" ? styles.active : "",
+        ].join(" ")}
+        href="/about"
+      >
+        About
+      </Link>
+    </nav>
+  );
+}
